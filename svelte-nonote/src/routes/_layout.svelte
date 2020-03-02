21ab1319f3569adb1ffac11b0c0fe2175061c0a5
benmcgarvey <ben@sidekicker.com.au>
2020-03-01 23:17:51 -0500

    svelte-header

--- svelte-nonote/src/routes/_layout.svelte
+++ svelte-nonote/src/routes/_layout.svelte
@@ -1,14 +1,62 @@
 <style>
   main {
     height: 100%;
-    position: relative;
     max-width: 56em;
     background-color: white;
     padding: 2em;
     margin: 0 auto;
     box-sizing: border-box;
   }
+
+  .Header {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .Content {
+    width: 80%;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .Content::after {
+    content: "";
+    flex: 1;
+  }
+
+  .Title {
+    flex: 1;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+  }
+
+  .DbAddress {
+    flex: 1;
+    align-items: center;
+  }
+
+  .DbAddress > input {
+    width: 100%;
+  }
 </style>
+
+<div class="Header">
+  <div class="Content">
+    <div class="Title">
+      <img src="ghost.svg" alt="cartoon ghost logo" height="55" width="55" />
+      <h1>no-note</h1>
+    </div>
+    <div class="DbAddress">
+      <input type="text" />
+    </div>
+  </div>
+
+</div>
 
 <main>
   <slot />
